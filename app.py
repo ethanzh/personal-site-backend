29d8eac44f0578027303f390e24dd2fb80cc6310
Ethan Houston <ethan.houston@gmail.com>
2018-02-24 22:51:20 -0800

    CORS

--- app.py
+++ app.py
@@ -1,24 +1,21 @@
 from flask import Flask, jsonify
-from flask.ext.cors import CORS, cross_origin
+from flask.ext.cors import CORS
 from sklearn import datasets, metrics
 from sklearn.tree import DecisionTreeClassifier
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+CORS(app)
 
 # Load Dataset from scikit-learn.
 dataset = datasets.load_iris()
 
 
 @app.route('/')
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def hello_world():
     return 'Hello World!'
 
 
 @app.route('/test')
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
 def hello():
     model = DecisionTreeClassifier()
     model.fit(dataset.data, dataset.target)
