9d6615a0be7c04bcc3ee28bccf46d99df396d198
Ethan Houston <ethan.houston@gmail.com>
2018-02-24 22:23:28 -0800

    gunicorn

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+from sklearn import datasets, svm
 
 app = Flask(__name__)
+
+# Load Dataset from scikit-learn.
+digits = datasets.load_digits()
 
 
 @app.route('/')
@@ -8,6 +12,13 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/test')
+def hello():
+    clf = svm.SVC(gamma=0.001, C=100.)
+    clf.fit(digits.data[:-1], digits.target[:-1])
+    prediction = clf.predict(digits.data[-1:])
+
+    return jsonify({'prediction': repr(prediction)})
 
 
 if __name__ == '__main__':
